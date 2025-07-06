f9bddc0d26e09742a1e09e72827ca19ed5fc0567
Gall-oDrone <piehavok@hotmail.com>
2025-07-06 17:02:07 -0600

    Widget Stack Gameplay Tags

---
+++ Source/FrontendUI/Public/FrontendGameplayTags.h
@@ -0,0 +1,13 @@
+// Diego Gallo All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FrontendGameplayTags
+{
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Modal);
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameMenu);
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_GameHud);
+	FRONTENDUI_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(Frontend_WidgetStack_Frontend);
+}
