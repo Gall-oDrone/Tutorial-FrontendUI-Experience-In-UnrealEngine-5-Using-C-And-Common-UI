9a91eb4c750523c0eb73965ea9b9bd5bd6673c85
Gall-oDrone <piehavok@hotmail.com>
2025-08-30 16:16:36 -0600

    Confirmation Screen

---
+++ Source/FrontendUI/Public/Widgets/Widget_ConfirmScreen.h
@@ -0,0 +1,30 @@
+// Diego Gallo All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Widget_ActivatableBase.h"
+#include "FrontendTypes/FrontendEnumTypes.h"
+#include "Widget_ConfirmScreen.generated.h"
+
+class UCommonTextBlock;
+class UDynamicEntryBox;
+
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNaiveTick))
+class FRONTENDUI_API UWidget_ConfirmScreen : public UWidget_ActivatableBase
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(meta = (BindWidget))
+	UCommonTextBlock* CommonTextBlock_Title;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonTextBlock* CommonTextBlock_Message;
+
+	UPROPERTY(meta = (BindWidget))
+	UDynamicEntryBox* DynamicEntryBox_Buttons;
+};
