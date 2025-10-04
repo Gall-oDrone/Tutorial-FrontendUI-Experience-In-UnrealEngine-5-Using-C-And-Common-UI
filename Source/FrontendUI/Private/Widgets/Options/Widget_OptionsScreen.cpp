a6278d5210b3e0b8da619d685685847d1e64f2ae
Gall-oDrone <piehavok@hotmail.com>
2025-10-04 17:53:13 -0600

    On Options Tab Selected

--- Source/FrontendUI/Private/Widgets/Options/Widget_OptionsScreen.cpp
+++ Source/FrontendUI/Private/Widgets/Options/Widget_OptionsScreen.cpp
@@ -32,6 +32,8 @@ void UWidget_OptionsScreen::NativeOnInitialized()
 			FSimpleDelegate::CreateUObject(this, &ThisClass::OnBackBoundActionTriggered)
 		)
 	);
+
+	TabListWidget_OptionsTabs->OnTabSelected.AddUniqueDynamic(this, &ThisClass::OnOptionsTabSelected);
 }
 
 void UWidget_OptionsScreen::NativeOnActivated()
@@ -77,4 +79,9 @@ void UWidget_OptionsScreen::OnResetBoundActionTriggered()
 void UWidget_OptionsScreen::OnBackBoundActionTriggered()
 {
 	DeactivateWidget();
+}
+
+void UWidget_OptionsScreen::OnOptionsTabSelected(FName TabId)
+{
+	Debug::Print(TEXT("New Tab Selected. Tab ID: ") + TabId.ToString());
 }
