e2fc6eccbfc0365779cffb43d1d28c180ec1227f
Gall-oDrone <piehavok@hotmail.com>
2025-10-12 15:31:50 -0600

    String Data Object

---
+++ Source/FrontendUI/Public/Widgets/Options/DataObjects/ListDataObject_Value.h
@@ -0,0 +1,17 @@
+// Diego Gallo All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/Options/DataObjects/ListDataObject_Base.h"
+#include "ListDataObject_Value.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTENDUI_API UListDataObject_Value : public UListDataObject_Base
+{
+	GENERATED_BODY()
+	
+};
