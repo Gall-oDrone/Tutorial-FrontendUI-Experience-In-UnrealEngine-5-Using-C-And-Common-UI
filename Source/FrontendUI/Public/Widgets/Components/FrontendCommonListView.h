0e0246a43773241a40a4f0e744f186b82b00b743
Gall-oDrone <piehavok@hotmail.com>
2025-11-01 15:48:43 -0600

    On Generate Entry Widget

--- Source/FrontendUI/Public/Widgets/Components/FrontendCommonListView.h
+++ Source/FrontendUI/Public/Widgets/Components/FrontendCommonListView.h
@@ -6,6 +6,7 @@
 #include "CommonListView.h"
 #include "FrontendCommonListView.generated.h"
 
+class UDataAsset_DataListEntryMapping;
 /**
  * 
  */
@@ -14,4 +15,17 @@ class FRONTENDUI_API UFrontendCommonListView : public UCommonListView
 {
 	GENERATED_BODY()
 	
+protected:
+	//~ Begin UCommonListView Interface
+	virtual UUserWidget& OnGenerateEntryWidgetInternal(UObject* Item, TSubclassOf<UUserWidget> DesiredEntryClass, const TSharedRef<STableViewBase>& OwnerTable) override;
+	//~ End UCommonListView Interface
+private:
+	//~ Begin UWidget Interface
+#if WITH_EDITOR	
+	virtual void ValidateCompiledDefaults(class IWidgetCompilerLog& CompileLog) const override;
+#endif
+	//~ End UWidget Interface
+
+	UPROPERTY(EditAnywhere, Category = "Frontend List View Settings")
+	UDataAsset_DataListEntryMapping* DataListEntryMapping;
 };
