8daa2229bdfefa6cd518ae62316e7ebe4eb094f9
Gall-oDrone <piehavok@hotmail.com>
2025-11-22 16:28:00 -0600

    Apply Settings

--- Source/FrontendUI/Public/Widgets/Options/DataObjects/ListDataObject_Base.h
+++ Source/FrontendUI/Public/Widgets/Options/DataObjects/ListDataObject_Base.h
@@ -36,6 +36,7 @@ public:
 	virtual TArray<UListDataObject_Base*> GetAllChildListData() const { return TArray<UListDataObject_Base*>(); }
 	virtual bool HasAnyChildListData() const { return false; }
 
+	void SetShouldApplySettingsImmediately(bool bShouldApplyRightAway) { bShouldApplyChangeImmediately = bShouldApplyRightAway; }
 protected:
 	//Empty in base class. The child classes should override it to handle the initialized needed accordingly
 	virtual void OnDataObjectInitialized();
@@ -51,4 +52,6 @@ private:
 
 	UPROPERTY(Transient)
 	UListDataObject_Base* ParentData;
+
+	bool bShouldApplyChangeImmediately = false;
 };
