da4e2dd3058b1235dbee87ec14d5730a8396cc2e
Gall-oDrone <piehavok@hotmail.com>
2026-04-04 18:29:45 -0600

    Resolution Data Object

---
+++ Source/FrontendUI/Private/Widgets/Options/DataObjects/ListDataObject_StringResolution.cpp
@@ -0,0 +1,9 @@
+// Diego Gallo All Rights Reserved
+
+
+#include "Widgets/Options/DataObjects/ListDataObject_StringResolution.h"
+
+void UListDataObject_StringResolution::InitResolutionValues()
+{
+
+}
