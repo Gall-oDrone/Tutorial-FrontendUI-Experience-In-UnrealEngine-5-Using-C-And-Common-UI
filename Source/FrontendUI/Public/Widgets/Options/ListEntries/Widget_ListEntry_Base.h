72908945c87eb65af222bf46f66785ee252f3013
Gall-oDrone <piehavok@hotmail.com>
2026-04-12 13:25:14 -0600

    Update Widget Editable State

--- Source/FrontendUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_Base.h
+++ Source/FrontendUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_Base.h
@@ -43,6 +43,8 @@ protected:
 	//The child class should override this function to update the UI values after the data object has been modified. Super call is not needed.
 	virtual void OnOwningListDataObjectModified(UListDataObject_Base* OwningModifiedData, EOptionsListDataModifyReason ModifyReason);
 
+	virtual void OnOwningDependencyDataObjectModified(UListDataObject_Base* OwningModifiedDependencyData, EOptionsListDataModifyReason ModifyReason);
+
 	//The child class should override this to change editable state of the widgets it owns. Super calls is expected.
 	virtual void OnToggleEditableState(bool bIsEditable);
 
@@ -53,4 +55,7 @@ private:
 	UPROPERTY(BlueprintReadOnly,meta = (BindWidgetOptional,AllowPrivateAccess = "true"))
 	UCommonTextBlock* CommonText_SettingDisplayName;
 	//***** Bound Widgets ***** //
+
+	UPROPERTY(Transient)
+	UListDataObject_Base* CachedOwningDataObject;
 };
