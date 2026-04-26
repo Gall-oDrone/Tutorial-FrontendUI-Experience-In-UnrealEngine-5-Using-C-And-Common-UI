a1f8e4588a2ba3eefc241846434343c3e76d0951
Gall-oDrone <piehavok@hotmail.com>
2026-04-26 17:47:14 -0600

    Update Button Image

--- Source/FrontendUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_KeyRemap.h
+++ Source/FrontendUI/Public/Widgets/Options/ListEntries/Widget_ListEntry_KeyRemap.h
@@ -7,6 +7,7 @@
 #include "Widget_ListEntry_KeyRemap.generated.h"
 
 class UFrontendCommonButtonBase;
+class UListDataObject_KeyRemap;
 /**
  * 
  */
@@ -15,11 +16,21 @@ class FRONTENDUI_API UWidget_ListEntry_KeyRemap : public UWidget_ListEntry_Base
 {
 	GENERATED_BODY()
 
+protected:
+	//~ Begin UWidget_ListEntry_Base Interface
+	virtual void OnOwningListDataObjectSet(UListDataObject_Base* InOwningListDataObject) override;
+	virtual void OnOwningListDataObjectModified(UListDataObject_Base* OwningModifiedData, EOptionsListDataModifyReason ModifyReason) override;
+	//~ End UWidget_ListEntry_Base Interface
+
 private:
+	//***** Bound Widgets ***** //
 	UPROPERTY(BlueprintReadOnly, meta = (BindWidget,AllowPrivateAccess = "true"))
 	UFrontendCommonButtonBase* CommonButton_RemapKey;
 	
 	UPROPERTY(BlueprintReadOnly, meta = (BindWidget,AllowPrivateAccess = "true"))
 	UFrontendCommonButtonBase* CommonButton_ResetKeyBinding;
-	
+	//***** Bound Widgets ***** //
+
+	UPROPERTY(Transient)
+	UListDataObject_KeyRemap* CachedOwningKeyRemapDataObject;
 };
