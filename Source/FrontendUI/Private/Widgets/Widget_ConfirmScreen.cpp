0949fb8e556caeaa8b30102de15d57c50f07c292
Gall-oDrone <piehavok@hotmail.com>
2026-05-10 15:10:16 -0600

    Reset Key Binding

--- Source/FrontendUI/Private/Widgets/Widget_ConfirmScreen.cpp
+++ Source/FrontendUI/Private/Widgets/Widget_ConfirmScreen.cpp
@@ -88,24 +88,7 @@ void UWidget_ConfirmScreen::InitConfirmScreen(UConfirmScreenInfoObject* InScreen
 
 	for (const FConfirmScreenButtonInfo& AvailableButtonInfo : InScreenInfoObject->AvailableScreenButtons)
 	{
-		FDataTableRowHandle InputActionRowHandle;
-
-		switch (AvailableButtonInfo.ConfirmScreenButtonType)
-		{
-		case EConfirmScreenButtonType::Cancelled:
-			InputActionRowHandle = ICommonInputModule::GetSettings().GetDefaultBackAction();
-			break;
-
-		case EConfirmScreenButtonType::Closed:
-			InputActionRowHandle = ICommonInputModule::GetSettings().GetDefaultBackAction();
-			break;
-
-		default:
-			break;
-		}
 		UFrontendCommonButtonBase* AddedButton = DynamicEntryBox_Buttons->CreateEntry<UFrontendCommonButtonBase>();
-		AddedButton->SetButtonText(AvailableButtonInfo.ButtonTextToDisplay);
-		AddedButton->SetTriggeringInputAction(InputActionRowHandle);
 		AddedButton->OnClicked().AddLambda(
 			[ClickedButtonCallback, AvailableButtonInfo,this]()
 			{
