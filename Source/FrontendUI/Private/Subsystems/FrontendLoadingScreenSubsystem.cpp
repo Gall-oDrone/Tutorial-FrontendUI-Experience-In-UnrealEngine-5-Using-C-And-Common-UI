84357f9d6d0e395dd8e6995b7f25b8c8468d3f1b
Gall-oDrone <piehavok@hotmail.com>
2026-05-23 17:31:10 -0600

    Tickable World Object

--- Source/FrontendUI/Private/Subsystems/FrontendLoadingScreenSubsystem.cpp
+++ Source/FrontendUI/Private/Subsystems/FrontendLoadingScreenSubsystem.cpp
@@ -30,6 +30,40 @@ bool UFrontendLoadingScreenSubsystem::Deinitialize()
 	FCoreUObjectDelegates::PostLoadMapWithWorld.RemoveAll(this);
 }
 
+UWorld* UFrontendLoadingScreenSubsystem::GetTickableGameObjectWorld() const
+{
+	if (UGameInstance* OwningGameInstance = GetGameInstance())
+	{
+		return OwningGameInstance->GetWorld();
+	}
+
+	return nullptr;
+}
+
+void UFrontendLoadingScreenSubsystem::Tick(float DeltaTime)
+{
+	Debug::Print(TEXT("Ticking"));
+}
+
+ETickableTickType UFrontendLoadingScreenSubsystem::GetTickableTickType() const
+{
+	if (IsTemplate())
+	{
+		return ETickableTickType::Never;
+	}
+	return ETickableTickType::Conditional;
+}
+
+bool UFrontendLoadingScreenSubsystem::IsTickable() const
+{
+	return GetGameInstance() && GetGameInstance()->GetGameViewportClient();
+}
+
+TStatId UFrontendLoadingScreenSubsystem::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(UFrontendLoadingScreenSubsystem,FStatGroup_STATGROUP_Tickables);
+}
+
 void UFrontendLoadingScreenSubsystem::OnMapPreloaded(const FWorldContext& WorldContext, const FString& MapName)
 {
 	Debug::Print(TEXT("On Map Pre Loaded"));
--- Source/FrontendUI/Public/Subsystems/FrontendLoadingScreenSubsystem.h
+++ Source/FrontendUI/Public/Subsystems/FrontendLoadingScreenSubsystem.h
@@ -10,7 +10,7 @@
  * 
  */
 UCLASS()
-class FRONTENDUI_API UFrontendLoadingScreenSubsystem : public UGameInstanceSubsystem
+class FRONTENDUI_API UFrontendLoadingScreenSubsystem : public UGameInstanceSubsystem, public FTickableGameObject
 {
 	GENERATED_BODY()
 	
@@ -20,6 +20,14 @@ public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection);
 	virtual void Deinitialize();
 	//~ End USubsystem Interface
+
+	//~ Begin FTickableGameObject Interface
+	virtual UWorld* GetTickableGameObjectWorld() const override;
+	virtual void Tick(float DeltaTime) override;
+	virtual ETickableTickType GetTickableTickType() const override;
+	virtual bool IsTickable() const override;
+	virtual TStatId GetStatId() const override;
+	//~ End FTickableGameObject Interface
 
 private:
 	void OnMapPreloaded(const FWorldContext& WorldContext, const FString& MapName);
