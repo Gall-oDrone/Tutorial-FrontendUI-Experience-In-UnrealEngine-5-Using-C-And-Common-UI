17aed9737bfa067703ce7738d35704512e79226b
Gall-oDrone <piehavok@hotmail.com>
2026-05-23 18:10:32 -0600

    Try Update Loading Screen

--- Source/FrontendUI/Private/Subsystems/FrontendLoadingScreenSubsystem.cpp
+++ Source/FrontendUI/Private/Subsystems/FrontendLoadingScreenSubsystem.cpp
@@ -42,7 +42,9 @@ UWorld* UFrontendLoadingScreenSubsystem::GetTickableGameObjectWorld() const
 
 void UFrontendLoadingScreenSubsystem::Tick(float DeltaTime)
 {
-	Debug::Print(TEXT("Ticking"));
+	//Debug::Print(TEXT("Ticking"));
+
+	TryUpdateLoadingScreen();
 }
 
 ETickableTickType UFrontendLoadingScreenSubsystem::GetTickableTickType() const
@@ -66,10 +68,46 @@ TStatId UFrontendLoadingScreenSubsystem::GetStatId() const
 
 void UFrontendLoadingScreenSubsystem::OnMapPreloaded(const FWorldContext& WorldContext, const FString& MapName)
 {
-	Debug::Print(TEXT("On Map Pre Loaded"));
+	/*Debug::Print(TEXT("On Map Pre Loaded"));*/
+
+	if (WorldContext.OwningGameInstance != GetGameInstance())
+	{
+		return;
+	}
+
+	SetTickableTickType(ETickableTickType::Conditional);
+
+	bIsCurrentlyLoadingMap = true;
+
+	TryUpdateLoadingScreen();
 }
 
 void UFrontendLoadingScreenSubsystem::OnMapPostLoaded(UWorld* LoadedWorld)
 {
-	Debug::Print(TEXT("On Map Post Loaded"));
+	/*Debug::Print(TEXT("On Map Post Loaded"));*/
+
+	if (LoadedWorld && LoadedWorld->GetGameInstance() == GetGameInstance())
+	{
+		bIsCurrentlyLoadingMap = false;
+	}
+}
+
+void UFrontendLoadingScreenSubsystem::TryUpdateLoadingScreen()
+{
+	//Check if there's any start up loading screen that's currently active
+
+	//Check if we should show the loading screen
+	if (true)
+	{
+		//Try display the loading screen here
+	}
+	else
+	{
+		//Try remove the current active loading screen
+
+		//Notify the loading is complete
+
+		//Disable the ticking
+		SetTickableTickType(ETickableTickType::Never);
+	}
 }
--- Source/FrontendUI/Public/Subsystems/FrontendLoadingScreenSubsystem.h
+++ Source/FrontendUI/Public/Subsystems/FrontendLoadingScreenSubsystem.h
@@ -33,4 +33,8 @@ private:
 	void OnMapPreloaded(const FWorldContext& WorldContext, const FString& MapName);
 
 	void OnMapPostLoaded(UWorld* LoadedWorld);
+
+	void TryUpdateLoadingScreen();
+
+	bool bIsCurrentlyLoadingMap = false;
 };
